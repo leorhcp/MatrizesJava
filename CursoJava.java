ae3ef654c7cc96de33bca7c9c6bb1a3cc221df46
Leonel <leorhcp1@gmail.com>
2023-04-27 23:06:17 -0300

    Adicionado loop simplificado

--- CursoJava.java
+++ CursoJava.java
@@ -6,19 +6,28 @@ public class CursoJava{
         final int linhas=3;
         final int colunas=5;
 
-        int[][] numeros=new int[linhas][colunas];
+        int[][] matriz=new int[linhas][colunas];
         
         for(int l=0; l<linhas;l++){
             for (int c=0; c<colunas;c++){
-                numeros[l][c]=new SecureRandom().nextInt(100);
+                matriz[l][c]=new SecureRandom().nextInt(100);
             }
         }
-
+        System.out.println("Matriz 01 - Loop For tradicional");
         for(int l=0; l<linhas;l++){
             for(int c=0;c<colunas;c++){
-                System.out.printf("%2d | ",numeros[l][c]);
+                System.out.printf("%2d | ",matriz[l][c]);
                 }
                 System.out.printf("%n");
+        }
+        System.out.println("\nMatriz 02 - Loop for simplificado:");
+
+        //for simplificado para arrays
+        for (int[] m:matriz){
+            for(int v:m){
+                System.out.printf("%2d | ", v);
+            }
+            System.out.printf("%n");
         }
        
 
